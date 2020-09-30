6cf09ac3aada4206cd3dfd064e403dd6c5fcc700
ddp <1178766740@qq.com>
2020-09-30 12:53:07 +0800

    Commit Message

--- src/org/lins/students/students.java
+++ src/org/lins/students/students.java
@@ -4,6 +4,8 @@ public class students {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(" I am a boy");
+		System.out.println("hello woorld");
 
 	}
 
