b4e68b4300f6cd4b1b59fc0204dfb0f31943f4b0
ddp1234 <72114236+ddp1234@users.noreply.github.com>
2020-10-07 10:31:55 +0800

    Create Tex.java

---
+++ src/org/lins/students/Tex.java
@@ -0,0 +1,13 @@
+package org.lins.students;
+
+public class students {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("456");
+		System.out.println("123");
+		
+
+	}
+
+}
